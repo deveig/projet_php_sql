575ccc8f35f41cbd717d5ccf419bd210d21a5a3d
deveig <cmdev@outlook.fr>
2024-05-13 12:05:53 +0200

    Ajout d'une balise meta pour optimisation mobile

--- view.php
+++ view.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/normalize.css" type="text/css" />
     <link rel="stylesheet" href="./css/recipe.css" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
