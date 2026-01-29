727f4e7b93ad53e02fd5cb755cc78acff81242aa
Celine <cmdev@outlook.fr>
2026-01-29 15:14:33 +0100

    Change workflow

--- model.php
+++ model.php
@@ -15,7 +15,7 @@ class DatabaseConnection
 
     public static function getConnection() {
         if(self::$instance === null) {
-            self::$dotenv = Dotenv::createImmutable(__DIR__);
+            self::$dotenv = Dotenv::createImmutable(__DIR__, ".env.prod");
             self::$dotenv->load();
             $db_url = $_ENV['DB_URL'];
             $db_username = $_ENV['DB_USERNAME'];
